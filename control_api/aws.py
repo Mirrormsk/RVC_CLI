6d8fddb53fc21c2642b7e046e8296adbbe716a86
Maxim Donchuk <m.donchuk@gmail.com>
2024-05-15 13:06:11 +0300

    add print

--- control_api/aws.py
+++ control_api/aws.py
@@ -23,6 +23,7 @@ class AWSService:
     def download_file(file_s3_url: str, path_to_save: str):
         parsed_url = urlparse(file_s3_url)
         file_s3_key = parsed_url.path.lstrip('/')
+        print(f"Downloading {file_s3_key} from {file_s3_url}")
         get_object_response = s3.get_object(Bucket=settings.aws_storage_bucket_name, Key=file_s3_key)
 
         file_content = get_object_response['Body'].read()
