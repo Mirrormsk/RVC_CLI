32d86c36f4d41ed916aa018fb3c4d8b6ec3934c4
Maxim Donchuk <m.donchuk@gmail.com>
2024-05-15 11:41:24 +0300

    update fields

--- control_api/config.py
+++ control_api/config.py
@@ -4,15 +4,19 @@ from typing import Optional, Dict, Any
 
 
 class AppSettings(BaseSettings):
-    aws_bucket: str = Field(env='AWS_STORAGE_BUCKET_NAME')
 
-    # admin_email: str = Field("admin@example.com", env='ADMIN_EMAIL')
-    # items_per_user: int = Field(100, env='ITEMS_PER_USER')
+    aws_access_key_id: str = Field(env='AWS_ACCESS_KEY_ID')
+    aws_secret_access_key: str = Field(env='AWS_SECRET_ACCESS_KEY')
+    aws_bucket: str = Field(env='AWS_STORAGE_BUCKET_NAME')
+    aws_s3_endpoint_url: Optional[str] = Field(default=None, env='AWS_S3_ENDPOINT_URL')
+    aws_s3_custom_domain: Optional[str] = Field(default=None, env='AWS_S3_CUSTOM_DOMAIN')
+    aws_s3_use_ssl: bool = Field(default=True, env='AWS_S3_USE_SSL')
+    queue_name: str = Field(env='QUEUE_NAME')
 
     class Config:
         env_file = '.env'
         env_file_encoding = 'utf-8'
-        extra = 'ignore'
+
 
 
 settings = AppSettings()
