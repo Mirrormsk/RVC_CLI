c56c9ac3050a284354f244cf5a57860981147116
Maxim Donchuk <m.donchuk@gmail.com>
2024-05-15 13:31:29 +0300

    add debug info

--- control_api/rvc_service.py
+++ control_api/rvc_service.py
@@ -8,8 +8,12 @@ from aws import AWSService
 from config import settings
 
 logger = logging.getLogger(__name__)
-logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
 
 
 class RVCService:
