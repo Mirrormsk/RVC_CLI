30a81469884070eb8302f70f2d0a57617d80642c
Maxim Donchuk <m.donchuk@gmail.com>
2024-05-15 11:38:01 +0300

    ignore extra

--- control_api/config.py
+++ control_api/config.py
@@ -5,12 +5,14 @@ from typing import Optional, Dict, Any
 
 class AppSettings(BaseSettings):
     aws_bucket: str = Field("AWS Bucket name", env='AWS_STORAGE_BUCKET_NAME')
+
     # admin_email: str = Field("admin@example.com", env='ADMIN_EMAIL')
     # items_per_user: int = Field(100, env='ITEMS_PER_USER')
 
     class Config:
         env_file = '.env'
         env_file_encoding = 'utf-8'
+        extra = 'ignore'
 
 
 settings = AppSettings()
