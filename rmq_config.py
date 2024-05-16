36f7081e81c0062bc4dc3b57cbc410d4eaea0f51
Maxim Donchuk <m.donchuk@gmail.com>
2024-05-16 17:33:02 +0300

    update config

--- rmq_config.py
+++ rmq_config.py
@@ -3,8 +3,9 @@ import os
 
 from config import settings
 
-rmq_parameters = pika.URLParameters(settings.rmq_connection_url)
-connection = pika.BlockingConnection(rmq_parameters)
 
-
-channel = connection.channel()
+def create_channel() -> tuple[pika.BlockingConnection, pika.adapters.blocking_connection.BlockingChannel]:
+    rmq_parameters = pika.URLParameters(settings.rmq_connection_url)
+    connection = pika.BlockingConnection(rmq_parameters)
+    channel = connection.channel()
+    return connection, channel
--- run_consuming.py
+++ run_consuming.py
@@ -1,14 +1,35 @@
 import json
 import os
+
+import pika
 from dotenv import load_dotenv
-from rmq_config import channel
+from rmq_config import create_channel
 from rvc_service import rvc_service
 import logging
+from config import settings
 
-
-load_dotenv(dotenv_path='.env')
 
 logger = logging.getLogger(__name__)
+
+
+def send_acknowledgment(properties):
+    connection = pika.BlockingConnection(pika.URLParameters(os.getenv('RABBITMQ_URL')))
+    ack_channel = connection.channel()
+    ack_queue_name = properties.reply_to
+
+    ack_message = {
+        'status': 'accepted',
+        'correlation_id': properties.correlation_id,
+    }
+
+    ack_channel.basic_publish(
+        exchange='',
+        routing_key=ack_queue_name,
+        body=json.dumps(ack_message),
+        properties=pika.BasicProperties(
+            correlation_id=properties.correlation_id
+        ))
+    connection.close()
 
 
 def callback(ch, method, properties, body):
@@ -16,22 +37,33 @@ def callback(ch, method, properties, body):
     logger.info('Received %r', json_data)
 
     try:
+        send_acknowledgment(properties)
+
         rvc_service.retrieve_command(command_data=json_data)
+
+        ch.basic_ack(delivery_tag=method.delivery_tag)
     except Exception as e:
         logging.error(e, exc_info=True)
+        ch.basic_nack(delivery_tag=method.delivery_tag)
 
 
-queue_name = os.getenv('QUEUE_NAME')
+def main():
+    connection, channel = create_channel()
 
+    channel.basic_consume(queue=settings.queue_name, on_message_callback=callback, auto_ack=False)
 
-channel.basic_consume(
-    queue=queue_name,
-    on_message_callback=callback,
-    auto_ack=True
-)
+    try:
+        logger.info('Waiting for messages')
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        channel.stop_consuming()
+    except pika.exceptions.StreamLostError as e:
+        logger.error(f'StreamLostError: {e}, retrying...')
+        main()
+    finally:
+        connection.close()
+
 
 if __name__ == '__main__':
+    main()
 
-    logger.info('Waiting for messages')
-    print('Waiting for messages')
-    channel.start_consuming()
