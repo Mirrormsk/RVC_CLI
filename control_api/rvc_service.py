0c67290566d29d572ab6d270fc5960f38ff1c605
Maxim Donchuk <m.donchuk@gmail.com>
2024-05-20 17:53:49 +0300

    add model downloading

--- control_api/rvc_service.py
+++ control_api/rvc_service.py
@@ -1,6 +1,8 @@
 import subprocess
 import logging
 import json
+
+import botocore
 import requests
 import os
 from typing import List
@@ -28,7 +30,7 @@ class RVCService:
         self.requests_retry = 5
         self.python_command = "python"
         self.main_py_path = "main.py"
-        self.s3_results_path = 'received_from_rvc'
+        self.s3_results_path = "received_from_rvc"
 
         for path in (
             self.files_for_process_dir,
@@ -47,6 +49,7 @@ class RVCService:
     ):
         """Insert model info in json file"""
         print(f"Adding model info for {model_name}")
+
         try:
             with open(self.data_file, "r+") as file:
                 fcntl.flock(file, fcntl.LOCK_EX)
@@ -106,6 +109,35 @@ class RVCService:
                 if response.status_code == 200:
                     break
 
+    def download_model(
+        self, model_name: str, pth_file_s3_path: str, index_file_s3_path: str
+    ):
+
+        pth_save_dir = self.logs_dir
+        index_save_dir = os.path.join(self.logs_dir, model_name)
+
+        pth_save_path = os.path.join(pth_save_dir, pth_file_s3_path.split("/")[-1])
+        index_save_path = os.path.join(
+            index_save_dir, index_file_s3_path.split("/")[-1]
+        )
+
+        if not os.path.exists(index_save_dir):
+            os.makedirs(index_save_dir)
+        try:
+            AWSService.download_file(pth_file_s3_path, pth_save_path)
+            AWSService.download_file(index_file_s3_path, index_save_path)
+        except IOError as e:
+            logger.error(f"IO error while download model: {e}", exc_info=True)
+        except Exception as ex:
+            logger.error(f"Error while download model: {ex}", exc_info=True)
+        else:
+            rvc_service.add_model_info(
+                model_name=model_name,
+                pth_path=pth_save_path,
+                index_path=index_save_path,
+            )
+            logger.info(f"Successfully downloaded model data: {model_name}")
+
     def send_model_info(
         self, model_name: str, model_status: str = None, current_epoch: int = None
     ) -> None:
@@ -148,7 +180,7 @@ class RVCService:
             return self.run_infer(
                 model_name=command_data["model_name"],
                 file_aws_url=command_data["file_aws_url"],
-                file_id=command_data["file_id"]
+                file_id=command_data["file_id"],
             )
         else:
             logger.warning(f"Unknown command: {command}")
@@ -386,7 +418,14 @@ class RVCService:
 
         logger.info(f"Training task finished! Listen for new messages")
 
-    def run_infer(self, model_name: str, file_aws_url: str, file_id: int):
+    def run_infer(
+        self,
+        model_name: str,
+        file_aws_url: str,
+        file_id: int,
+        pth_file_s3_path: str,
+        index_file_s3_path: str,
+    ):
         filename = file_aws_url.rsplit("/", maxsplit=1)[-1]
         full_path = os.path.join(self.files_for_process_dir, filename)
 
@@ -399,8 +438,20 @@ class RVCService:
         model_data = rvc_service.get_model_data(model_name=model_name)
 
         if model_data is None:
-            logger.error(f"Model data not found: {model_name}")
-            return
+            logger.error(f"Model data not found locally: {model_name}")
+            try:
+                self.download_model(
+                    model_name=model_name,
+                    pth_file_s3_path=pth_file_s3_path,
+                    index_file_s3_path=index_file_s3_path,
+                )
+            except Exception as ex:
+                logger.error(
+                    f"Error while trying to download model info: {ex}", exc_info=True
+                )
+                return
+            else:
+                model_data = rvc_service.get_model_data(model_name=model_name)
 
         return_code, stderr = self.run_infer_command(
             input_path=full_path,
@@ -410,9 +461,7 @@ class RVCService:
         )
 
         if return_code != 0:
-            logger.error(
-                f"File processing failed, stop execution. Errors: {stderr}"
-            )
+            logger.error(f"File processing failed, stop execution. Errors: {stderr}")
             return
         else:
             logger.info("File processing succeeded")
